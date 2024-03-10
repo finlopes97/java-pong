4376276c62370f2591079944841278716362fba3
finlopes97 <Kyle.Finlayson1997@gmail.com>
2024-03-10 17:40:28 +1000

    feat(gfx): add ball to game window

---
+++ src/Ball.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+
+public class Ball {
+    private int x, y, cx, cy, speed, size;
+    private Color color;
+
+    public Ball(int x, int y, int cx, int cy, int speed, int size, Color color) {
+        this.x = x;
+        this.y = y;
+        this.cx = cx;
+        this.cy = cy;
+        this.speed = speed;
+        this.size = size;
+        this.color = color;
+    }
+
+    public void paint(Graphics graphics) {
+        graphics.setColor(color);
+        graphics.fillOval(x, y, size, size);
+    }
+}
--- src/PongGame.java
+++ src/PongGame.java
@@ -3,6 +3,7 @@ import java.awt.*;
 
 public class PongGame extends JPanel {
     static final int WINDOW_WIDTH = 640, WINDOW_HEIGHT = 480;
+    private Ball gameBall = new Ball(300, 200, 3, 3, 3, 10, Color.YELLOW);
 
     /**
      * Updates and draws all graphics on screen
@@ -10,5 +11,8 @@ public class PongGame extends JPanel {
     public void paintComponent(Graphics graphics) {
         graphics.setColor(Color.BLACK);
         graphics.fillRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+        gameBall.paint(graphics);
     }
+
 }
